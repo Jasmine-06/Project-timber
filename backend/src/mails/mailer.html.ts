cac4882e0653f83a94a7e8dd6c39c049d39f3289
Ashish Jangde <ashishjangde2003@gmail.com>
2025-11-27 17:22:44 +0530

    added mails and service

---
+++ backend/src/mails/mailer.html.ts
@@ -0,0 +1,25 @@
+const VerificationHtml = (name: string, verificationCode: string) => {
+  return `
+  <div style="font-family:Roboto,Arial; max-width:500px; margin:auto; padding:20px; border:1px solid #d0d7de; border-radius:10px;">
+      <h2 style="text-align:center; color:#0a66c2;">Email Verification</h2>
+
+      <p style="color:#333;">Hi <strong>${name}</strong>,</p>
+      <p style="color:#333;">Please enter the following verification code:</p>
+
+      <div style="text-align:center; margin:20px 0;">
+          <span style="display:inline-block; font-size:30px; font-weight:600; color:#0a66c2; padding:10px 20px; border:2px solid #0a66c2; border-radius:8px; letter-spacing:5px;">
+              ${verificationCode}
+          </span>
+      </div>
+
+      <p style="color:#555;">If this wasn’t you, kindly ignore the email.</p>
+      <p style="margin-top:30px; color:#777;">Best regards,<br>Your App Team</p>
+  </div>
+  `
+}
+
+
+
+export {
+    VerificationHtml
+}
