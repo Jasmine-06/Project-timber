c6c6fe072fd4790b7896824ee881c9988ef80ce8
gautambamne <gautam2003b@gmail.com>
2025-12-11 16:40:06 +0530

    Suspended account page

--- frontend/src/app/(public)/layout.tsx
+++ frontend/src/app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "./_components/AppSidebar";
 import { SiteHeader } from "./_components/SiteHeader";
 import { MobileNav } from "./_components/MobileNav";
+import { SuspendedAccountGuard } from "@/components/guards/SuspendedAccountGuard";
 
 export default function RootLayout({
   children,
@@ -9,15 +10,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col gap-4 p-4 md:px-4 md:py-6 mx-auto w-full max-w-[1100px] mb-14 md:mb-0">
-          {children}
-        </div>
-        <MobileNav />
-      </SidebarInset>
-    </SidebarProvider>
+    <SuspendedAccountGuard>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col gap-4 p-4 md:px-4 md:py-6 mx-auto w-full max-w-[1100px] mb-14 md:mb-0">
+            {children}
+          </div>
+          <MobileNav />
+        </SidebarInset>
+      </SidebarProvider>
+    </SuspendedAccountGuard>
   );
 }
